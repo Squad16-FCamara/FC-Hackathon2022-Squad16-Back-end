eddf07f8f55f7b4426d652a03e146905bc1d5c6a
Juliana Satoko Hagiwara Miura <hagiwaramiura.satoko@gmail.com>
2022-04-07 14:32:01 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import routes from './routes';
+import { DataSource } from 'typeorm';
+config();
+
+const appDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: true,
+  synchronize: true,
+  logging: false,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT || 3333;
+
+(async function start() {
+  await appDataSource
+    .initialize()
+    .then(() => console.log('Connected to database'))
+    .catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
+  app.listen(port, () => console.log('Server started'));
+})();
