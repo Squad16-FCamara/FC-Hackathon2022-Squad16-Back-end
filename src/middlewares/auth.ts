20fbccaab046cc2a5608af9715fa78979ac995bf
Juliana Satoko Hagiwara Miura <hagiwaramiura.satoko@gmail.com>
2022-04-08 12:55:58 -0300

    Create auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { decode, JwtPayload, verify } from 'jsonwebtoken';
+import AppError from '../errors/appError';
+
+async function authMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const [, token] = request.headers.authorization.split(' ');
+
+  if (!token) {
+    throw new AppError('Token not provided');
+  }
+
+  const secret = process.env.AUTH_SECRET || 'secret';
+  const { sub } = verify(token, secret) as JwtPayload;
+
+  request.user = { id: sub };
+
+  return next();
+}
+
+export default authMiddleware;
